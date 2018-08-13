2d8a9b76e4128945a0ad9f9acd0741eb1f9a1fd9
Pedro Rivera <pedrorivera@pedros-macbook-pro.local>
2018-08-13 14:42:12 -0400

    Initial Commit

---
+++ SideWorks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SideWorks
+//
+//  Created by Pedro Rivera on 8/13/18.
+//  Copyright © 2018 sadcrow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
